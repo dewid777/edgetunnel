692f98ca9ab6ef5a9fb9e2b4e777dd3e9e7d607e
dewid777 <137115346+dewid777@users.noreply.github.com>
2024-03-07 12:17:37 +0500

    Update worker-with-socks5-experimental.js

--- src/worker-with-socks5-experimental.js
+++ src/worker-with-socks5-experimental.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'b7625108-80fb-4544-a1f2-aa40c931aa52';
 
 let proxyIP = '';
 
